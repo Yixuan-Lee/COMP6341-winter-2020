f5e5d20702f171dd5f4fe2319fd3b5a4f6074556
liyixuan <liyixuan4030614@gmail.com>
2020-01-11 17:07:10 -0500

    assignment 1 part 1 done

--- assignment1/README.md
+++ assignment1/README.md
@@ -1,8 +1,19 @@
-References:
+# References:
 
 1. [opencv cv::filter2D documentation](https://docs.opencv.org/3.4/d4/d86/group__imgproc__filter.html#ga27c049795ce870216ddfb366086b5a04)
 
 2. [opencv cv::filter2D example 1](https://opencv-python-tutroals.readthedocs.io/en/latest/py_tutorials/py_imgproc/py_filtering/py_filtering.html)
 
 3. [opencv cv::filter2D example 2](https://www.programcreek.com/python/example/89373/cv2.filter2D)
+
+
+## Problem solving queries
+
+1. [np.dstack](https://docs.scipy.org/doc/numpy-1.13.0/reference/generated/numpy.dstack.html)
+
+2. [Numpy Data types](https://docs.scipy.org/doc/numpy-1.13.0/user/basics.types.html)
+
+3. [opencv cv::imshow doc](https://docs.opencv.org/3.0-beta/modules/highgui/doc/user_interface.html#imshow)
+
+
 
--- assignment1/src/main.py
+++ assignment1/src/main.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+from matplotlib import pyplot as plt
 
 
 # global variables
@@ -66,7 +67,7 @@ def separate_channels(bayer_image):
                 blue_channel[row, col] = bayer_image[row, col]
             elif row % 2 == 1 and col % 2 == 1:
                 green_channel[row, col] = bayer_image[row, col]
-            else:
+            elif (row % 2 == 0 and col % 2 == 1) or (row % 2 == 1 and col % 2 == 0):
                 red_channel[row, col] = bayer_image[row, col]
 
     return blue_channel, green_channel, red_channel
@@ -80,32 +81,74 @@ def get_channel_kernels():
     """
     blue_kernel = np.array([
         [1/4, 1/2, 1/4],
-        [1/2, 1  , 1/2],
-        [1/4, 1/2, 1/4]])
+        [1/2, 1,   1/2],
+        [1/4, 1/2, 1/4]], dtype=np.float32)
     green_kernel = np.array([
         [1/4, 1/2, 1/4],
-        [1/2, 1  , 1/2],
-        [1/4, 1/2, 1/4]])
+        [1/2, 1,   1/2],
+        [1/4, 1/2, 1/4]], dtype=np.float32)
     red_kernel = np.array([
-        [0  , 1/4, 0  ],
-        [1/4, 0  , 1/4],
-        [0  , 1/4, 0  ]])
+        [0,   1/4, 0],
+        [1/4, 1,   1/4],
+        [0,   1/4, 0]], dtype=np.float32)
 
     return blue_kernel, green_kernel, red_kernel
 
 
-def get_demosaic_channels(bayer_image, blue_channel, green_channel, red_channel, blue_kernel, green_kernel, red_kernel):
+def get_demosaic_channels(blue_channel, green_channel, red_channel, blue_kernel, green_kernel, red_kernel):
     """
     demosaicing 3 channels then return
 
     :return: demosaic_blue_channel, demosaic_green_channel, demosaic_red_channel
     """
     # 2D convolution
-    demosaic_blue_channel = cv2.filter2D(src=bayer_image, ddepth=-1, kernel=blue_kernel)
-    demosaic_green_channel = cv2.filter2D(src=bayer_image, ddepth=-1, kernel=green_kernel)
-    demosaic_red_channel = cv2.filter2D(src=bayer_image, ddepth=-1, kernel=red_channel)
+    demosaic_blue_channel = cv2.filter2D(src=blue_channel, ddepth=-1, kernel=blue_kernel)
+    demosaic_green_channel = cv2.filter2D(src=green_channel, ddepth=-1, kernel=green_kernel)
+    demosaic_red_channel = cv2.filter2D(src=red_channel, ddepth=-1, kernel=red_kernel)
 
     return demosaic_blue_channel, demosaic_green_channel, demosaic_red_channel
+
+
+def get_squared_differences(original, demosaic):
+    """
+    compute the squared differences between original and demosaic over 3 color channels
+
+    :param original: original image from JPG image
+    :param demosaic: demosaic image from BMP image
+    :return: squared differences of two images
+    """
+    return np.square(original - demosaic)
+
+
+def part_one_show(original, demosaic_blue_channel, demosaic_green_channel, demosaic_red_channel):
+    """
+    show the 3 images output described in guideline Part 1
+
+    :param original:                JPG image (shape of (height, width, no. of channels))
+    :param demosaic_blue_channel:   demosaic blue channel
+    :param demosaic_green_channel:  demosaic green channel
+    :param demosaic_red_channel:    demosaic red channel
+    """
+    # stack B/G/R channels together
+    demosaic = np.dstack((demosaic_blue_channel, demosaic_green_channel, demosaic_red_channel))
+
+    # compute the squared differences between the original and the demosaic
+    squared_diff = get_squared_differences(original, demosaic)
+
+    # important for cv2.imshow!!
+    # convert to unsigned int8 type so that the cv2.imshow will show using
+    # range [0, 255] instead of [0, 1]
+    demosaic = demosaic.astype(np.uint8)
+    squared_diff = squared_diff.astype(np.uint8)
+
+    # concatenate the 3 images horizontally
+    concat = np.hstack((original, demosaic, squared_diff))
+
+    cv2.imshow(winname='part 1 output', mat=concat)
+    # cv2.imshow('original', original)
+    # cv2.imshow('demosaic', demosaic)
+    # cv2.imshow('squared difference', squared_diff)
+    cv2.waitKey(0)
 
 
 def main():
@@ -134,14 +177,12 @@ def main():
     # demosaicing
     demosaic_blue_channel, demosaic_green_channel, demosaic_red_channel = \
         get_demosaic_channels(
-            bayer,
             blue_channel, green_channel, red_channel,
             blue_kernel, green_kernel, red_kernel)
 
-    print(demosaic_blue_channel)
-    print(demosaic_green_channel)
-    print(demosaic_red_channel)
-
+    # show the concatenated 3 output images in part 1
+    part_one_show(original, demosaic_blue_channel, demosaic_green_channel,
+        demosaic_red_channel)
 
 
 if __name__ == '__main__':
