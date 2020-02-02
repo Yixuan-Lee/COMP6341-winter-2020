9c4beb9063e0c25c97541e7bd9a706c322973858
liyixuan <liyixuan4030614@gmail.com>
2020-02-02 03:02:21 -0500

    add more comments

--- assignment2/src/feature_matching.py
+++ assignment2/src/feature_matching.py
@@ -10,7 +10,6 @@ def feature_distance(descriptor_1, descriptor_2):
     :param descriptor_2: descriptor of interest point 2
     :return: sum of difference between 2 descriptors
     """
-
     return np.sum(np.square(descriptor_1.get_descriptor()
                             - descriptor_2.get_descriptor()))
 
@@ -18,6 +17,18 @@ def feature_distance(descriptor_1, descriptor_2):
 def descriptors_matching(descriptor_dict_image_1, descriptor_dict_image_2,
         dmatch_list, ip_match_image_1, ip_match_image_2,
         ssd_threhold, ratio_test):
+    """
+    find the matching and store the DMatch in dmatch_list
+
+    :param descriptor_dict_image_1: descriptor dictionary of image 1
+    :param descriptor_dict_image_2: descriptor dictionary of image 2
+    :param dmatch_list:             a list which stores DMatches
+    :param ip_match_image_1:        a list which stores matched interest points in image 1
+    :param ip_match_image_2:        a list which stores matched interest points in image 2
+    :param ssd_threhold:            SSD distance threshold
+    :param ratio_test:              ratio threshold (best/second_best)
+    """
+    # initialize variables
     best_ip_1 = None
     best_ip_2 = None
     ip_match_idx = 0
@@ -35,7 +46,7 @@ def descriptors_matching(descriptor_dict_image_1, descriptor_dict_image_2,
                 continue
 
             if ssd_dist < best_ssd_dist:
-                # set best -> second best
+                # set best -> second_best
                 second_best_ssd_dist = best_ssd_dist
 
                 # set current -> best
@@ -45,18 +56,20 @@ def descriptors_matching(descriptor_dict_image_1, descriptor_dict_image_2,
             elif ssd_dist < second_best_ssd_dist:
                 second_best_ssd_dist = ssd_dist
 
-        # filter out by ratio
+        # filter matches by comparing the ratio
         ratio = best_ssd_dist / second_best_ssd_dist
         if ratio > ratio_test:
             # meaning best and second best are very similar, so we discard
-            # the match (associate with the 'fence' example)
+            # the match (associate with the 'fence' example in slide)
             continue
         else:
+            # append the matched interest points in image_1 and image_2 to
+            # 2 lists correspondingly
             ip_match_image_1.append(best_ip_1)
             ip_match_image_2.append(best_ip_2)
 
-            match = cv.DMatch(ip_match_idx, ip_match_idx, 5)
+            # append the match to the match list
+            match = cv.DMatch(ip_match_idx, ip_match_idx, 2)
             dmatch_list.append(match)
 
             ip_match_idx += 1
-
